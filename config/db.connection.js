eba77141f3358842795008208ad60d89ba616366
scarletknight13 <lamarbroxton@gmail.com>
2022-04-26 02:52:55 -0500

    initial commit

---
+++ config/db.connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const connectionStr = process.env.MONGODB_URL;
+
+mongoose.connect(connectionStr);
+
+mongoose.connection.on('connected', () => {
+    console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected... 🙌 🙌 🙌`)
+})
+
+mongoose.connection.on('error', (error) => {
+    console.log('MongoDB connection error ', error)
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected ⚡️ 🔌 ⚡️')
+})
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+const methodOverride = require('method-override')
+
+const controllers = require('./controllers')
+
+const PORT = 4000;
+
+require('./config/db.connection')
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+
+app.use(methodOverride('_method'))
+
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (request, response) => response.send('Server is up and running'))
+
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
