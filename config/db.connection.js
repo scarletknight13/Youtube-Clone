d335e96c3a8ca7885cb90ff0ad16e4c1fae821c8
cst7785 <cestevens95@gmail.com>
2022-04-27 11:00:27 -0400

    Removed dev test code for push.

--- config/db.connection.js
+++ config/db.connection.js
@@ -5,28 +5,7 @@ require('dotenv').config();
 const connectionStr = process.env.MONGODB_URI;
 
 mongoose.connect(connectionStr);
-const db = require('../models');
-// db.Video.create({
-//     title: {
-//     },
-//     videoData: {
-//     },
-//     channel: {
-//     },
-//     description: {
-//     },
-//     views: {
-//     },
-//     likes: {
-//     },
-//     comments: [{
-//     }])
-db.Channel.create({
-    name: "World Dominating AIs",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQABTJ8MQW3_0us05riwtEV_SCUCGqdyD9Izg&usqp=CAU",
-    subscribers: 856732
-})
-// new comment
+
 
 mongoose.connection.on('connected', () => {
     console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected.`)
