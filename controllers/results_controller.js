e79b2a146038d1c7e15a0f46c214518ee6a9b6ad
scarletknight13 <lamarbroxton@gmail.com>
2022-04-27 15:53:34 -0500

    added delete and create commment route

--- controllers/results_controller.js
+++ controllers/results_controller.js
@@ -6,30 +6,10 @@ const db = require('../models');
 
 router.get('/', async (req, res, next)=>{
     try{
-        const search = req.query.search_query;
-        let searchTags = search.split(' ');
-        const tagSet = new Set(searchTags)
-        console.log(searchTags);
-        const videos = await db.Video.find({})
-        videos.sort((a, b) =>{
-            let aTags = a.description.split(' ');
-            let bTags = b.description.split(' ');
-            console.log(aTags);
-            let aMatches = 0;
-            let bMatches = 0;
-            for(let tag of aTags)
-                if(tagSet.has(tag))
-                    ++aMatches;
-            
-            for(let tag of bTags)
-                if(tagSet.has(tag))
-                    ++bMatches;
-            return bMatches - aMatches;
-        })
-        const context = {videos};
-
-        res.send(videos.slice(2));
-        // return res.render('results.ejs', context);
+        const videos = await db.Video.find({});
+        let search = req.query.search_query
+        let searchResult = require('../scripts/search_algorithm')(search, videos);
+        res.send(videos);
     }
     catch(error){
         console.log(error);
