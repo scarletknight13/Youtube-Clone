0d1d2e59f2147fdeaed1d872ca47013cae631f58
kaeriv93 <vsan93@yahoo.com>
2022-04-29 15:31:35 -0500

    more javascript to display edit and delete button when pin is clicked

--- public/watch.js
+++ public/watch.js
@@ -3,7 +3,17 @@ let $comment = $('#addingcomment')
 
 let $commentButton = $('.comment-button')
 
+let $editPin = $('.edit-pin')
 
+let $editDelete = $('.edit-delete')
+
+
+
+$editPin.click(()=>{
+    $editPin.addClass('hidden')
+    $editDelete.removeClass('hidden')
+
+})
 
 $comment.click(()=>{
     $commentButton.removeClass('hidden')
