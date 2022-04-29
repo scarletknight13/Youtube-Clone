30650151065ed3df138d9e5029f1baaa6e9d6eed
cst7785 <cestevens95@gmail.com>
2022-04-29 13:27:16 -0400

    Created header.css and sideBar.css to isolate changes from the rest of the css files to eliminate code duplication/variation amongst the main.css/results.css/show.css. Added import statements to those files.

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
+//Import modules here
 const express = require('express');
-
 const app = express();
-
 const methodOverride = require('method-override')
-
 const controllers = require('./controllers')
 
+//Define Global Variables here
 const PORT = 4500;
-
 require('./config/db.connection')
 
+
+//Define Express Settings Here
 app.set('view engine', 'ejs')
-
 app.use(express.static('public'))
-
-
 app.use(methodOverride('_method'))
-
 app.use(express.urlencoded({ extended: false }))
 
 
-
+// Define Router Controllers here:
 app.use('/', controllers.home);
 app.use('/watch', controllers.watch);
 app.use('/results', controllers.results);
